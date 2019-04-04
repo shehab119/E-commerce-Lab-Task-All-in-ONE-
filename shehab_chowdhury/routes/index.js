c5d8a5bdf5626b072659b8c3cada84089a4d5d9e
Panda <shehab63363@gmail.com>
2019-04-04 16:02:05 +0600

    task 2

--- shehab_chowdhury/routes/index.js
+++ shehab_chowdhury/routes/index.js
@@ -5,8 +5,35 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+/* Task 1 Started From Here */
 router.get('/task1', function(req, res, next) {
   res.render('task1');
 });
+/* Ends of Task 1*/
+
+
+/* Task 2 Started From Here*/
+router.get('/task2', function(req, res, next) {
+  res.render('task2');
+});
+
+router.get('/task2p1', function(req, res, next) {
+  res.render('task2p1');
+});
+
+router.get('/task2p2', function(req, res, next) {
+  res.render('task2p2');
+});
+router.get('/task2p3', function(req, res, next) {
+  res.render('task2p3');
+});
+
+router.get('/task2p4', function(req, res, next) {
+  res.render('task2p4');
+});
+router.get('/task2p5', function(req, res, next) {
+  res.render('task2p5');
+});
+/* Ends of Task 2*/
+
 module.exports = router;
